009655ce14846fcd87d4f98bd994722bc18b97c7
daebenji <ev.fools@gmail.com>
2018-09-03 10:45:05 +0200

    foobar

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,5 @@
 
 apt-get update
 apt-get install -y vim net-tools apt-utils apt-transport-https curl procps perl-modules openssh-server whois git
+
+git clone git@github.com:daebenji/cfengine_masterfiles.git /var/cfengine/masterfiles/
